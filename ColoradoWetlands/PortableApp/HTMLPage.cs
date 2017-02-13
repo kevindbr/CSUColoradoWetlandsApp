a3ff74dd1145d384f2483ad1280297f4aeb0d540
Tim Kleier <timkleier@gmail.com>
2017-02-13 13:40:09 -0700

    Added dynamic rendering of HTML pages

---
+++ ColoradoWetlands/PortableApp/HTMLPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.IO;
+
+using Xamarin.Forms;
+
+namespace PortableApp
+{
+    public class HTMLPage : ContentPage
+    {
+        public HTMLPage(string file)
+        {
+            // Get file from PCL--in order for HTML files to be automatically pulled from the PCL, they need to be in a Views/HTML folder
+            var assembly = typeof(HTMLPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("PortableApp.Views.HTML." + file);
+            string text = "";
+            using (var reader = new System.IO.StreamReader(stream)) { text = reader.ReadToEnd(); }
+
+            // Generate WebView content
+            var browser = new WebView();
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.Html = @text;
+            browser.Source = htmlSource;
+            Content = browser;
+        }
+    }
+}
