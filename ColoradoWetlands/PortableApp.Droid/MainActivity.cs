814f8c540a57f9241c213e8eb6dce248fd7d1e2d
Tim Kleier <timkleier@gmail.com>
2017-04-10 11:11:08 -0600

    Updated Android app icons and application name

--- ColoradoWetlands/PortableApp.Droid/MainActivity.cs
+++ ColoradoWetlands/PortableApp.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace PortableApp.Droid
 {
-    [Activity(Label = "PortableApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Colorado Wetlands", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
