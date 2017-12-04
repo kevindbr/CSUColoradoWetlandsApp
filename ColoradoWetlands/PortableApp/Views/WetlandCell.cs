9a25c1aae2fe643a037c0f220e433395ec541948
Kenneth Conner <ksconner81@yahoo.com>
2017-12-04 15:43:12 -0700

    Wetland Cell

---
+++ ColoradoWetlands/PortableApp/Views/WetlandCell.cs
@@ -0,0 +1,35 @@
+﻿using FFImageLoading.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using PortableApp.Models;
+
+namespace PortableApp.Views
+{
+    public class WetlandCell : ViewCell
+    {
+        readonly CachedImage cachedImage = null;
+
+        public WetlandCell()
+        {
+            cachedImage = new CachedImage();
+            View = cachedImage;
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            var item = BindingContext as WetlandImage;
+
+            if (item == null)
+                return;
+
+            cachedImage.Source = item.ImageUrl;
+
+            base.OnBindingContextChanged();
+        }
+    }
+
+}
